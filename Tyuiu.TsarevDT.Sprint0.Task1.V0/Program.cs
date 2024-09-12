17696db34a30e03db560ac03fdb6b911aaa49dc4
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-09-12 17:11:07 +0500

    Add files via upload
    Task1

---
+++ Tyuiu.TsarevDT.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TsarevDT.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
