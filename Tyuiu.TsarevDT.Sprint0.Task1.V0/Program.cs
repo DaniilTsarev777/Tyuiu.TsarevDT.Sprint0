aca5d42a7a3fb47f619eb2df53be77bdf8380d44
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-09-12 17:36:44 +0500

    Add files via upload
    Task1

---
+++ Tyuiu.TsarevDT.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.TsarevDT.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Danya";
+            int age = 18;
+            Console.WriteLine("Hello my name is "+name+" Im "+age+" years old");
+            Console.ReadKey();
+        }
+    }
+}
